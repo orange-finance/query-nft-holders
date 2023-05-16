32853aaf44910950cd72f92ca535897f59fd0d10
shinobu1729 <sino1729defi@gmail.com>
2023-05-16 17:50:56 +0700

    add honey comb

--- main.js
+++ main.js
@@ -11,15 +11,20 @@ const alchemy = new Alchemy(config);
 const main = async () => {
   // Contract address
   const address = "0xa20cf9b0874c3e46b344deaeea9c2e0c3e1db37d";
+  const address2 = "0xCB0477d1Af5b8b05795D89D59F4667b59eAE9244";
 
   // Get owners
-  const owners = await alchemy.nft.getOwnersForContract(address);
-  await writeListToFile(owners, "list.json");
+  const owners1 = await alchemy.nft.getOwnersForContract(address);
+  const owners2 = await alchemy.nft.getOwnersForContract(address2);
+
+  await writeListToFile(owners1, "honey_jar.json");
+  await writeListToFile(owners2, "honey_comb.json");
 };
 
 async function writeListToFile(list, fileName) {
   try {
-    const jsonString = JSON.stringify(list, null, 2);
+    const convertedJson = list.owners.map((address) => ({ address }));
+    const jsonString = JSON.stringify(convertedJson, null, 2);
     await fs.writeFile(fileName, jsonString);
     console.log(`list have been saved to ${fileName}`);
   } catch (error) {
