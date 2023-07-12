429039721ed5c374256568415e5160513678121e
shinobu1729 <sino1729defi@gmail.com>
2023-07-12 13:48:12 +0900

    add new addresses

---
+++ scripts/getMerkleRoot.ts
@@ -0,0 +1,28 @@
+import { ethers } from "ethers";
+import { MerkleTree } from "merkletreejs";
+import { keccak256 } from "js-sha3";
+import * as fs from "fs";
+import * as path from "path";
+
+async function hashed(target: any[]) {
+  return target.map(({ address }) => {
+    return ethers.solidityPackedKeccak256(["address"], [address]);
+  });
+}
+
+async function main() {
+  const jsonFilePath = path.resolve(__dirname, "../WL/Product.json"); //update here to change list to import
+  const jsonData = fs.readFileSync(jsonFilePath, "utf-8");
+  const list = JSON.parse(jsonData);
+
+  const leaves = await hashed(list);
+  const tree = await new MerkleTree(leaves, keccak256, { sort: true });
+  const root = await tree.getHexRoot();
+
+  console.log("root:", root);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
